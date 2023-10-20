0f92223800ca3622ce56a4fbcfc3fe41e741d2d3
foomi <foomi.business@gmail.com>
2023-10-20 14:38:20 -0500

    delete api

--- api.js
+++
@@ -1,9 +0,0 @@
-// api.js
-export function fetchTodos() {
-    return fetch('/api/todos')
-      .then((response) => response.json())
-      .catch((error) => {
-        throw error;
-      });
-  }
-  
